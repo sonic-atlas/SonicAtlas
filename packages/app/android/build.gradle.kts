b5b6f83c0d660958c5de4d6f7385e798b54f67ba
Maximilian Berkes <heggo@heggo.dev>
2026-01-19 23:36:56 +0100

    fix: silence options warning in flutter android build

--- packages/app/android/build.gradle.kts
+++ packages/app/android/build.gradle.kts
@@ -3,6 +3,9 @@ allprojects {
         google()
         mavenCentral()
     }
+    tasks.withType<JavaCompile>().configureEach {
+        options.compilerArgs.add("-Xlint:-options")
+    }
 }
 
 val newBuildDir: Directory =
