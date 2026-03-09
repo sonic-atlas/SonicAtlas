8694c3474f1b35aaa3eeb636a32d28b82fe9b256
Maximilian Berkes <heggo@heggo.dev>
2026-03-09 21:45:09 +0100

    chore: upgrade dependencies

--- packages/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import audio_service
 import audio_session
 import file_picker
-import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
 
@@ -16,7 +15,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
