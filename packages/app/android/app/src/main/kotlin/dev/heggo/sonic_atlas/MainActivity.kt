6f474b7cc13343163ada3578b065e12418fd3898
ultraslayyy <ultra@ultraslayyy.xyz>
2026-04-12 01:35:25 +1000

    feat: hehe

--- packages/app/android/app/src/main/kotlin/dev/heggo/sonic_atlas/MainActivity.kt
+++ packages/app/android/app/src/main/kotlin/dev/heggo/sonic_atlas/MainActivity.kt
@@ -1,5 +1,20 @@
 package dev.heggo.sonic_atlas
 
 import com.ryanheise.audioservice.AudioServiceActivity
+import java.io.File
+import android.os.Bundle
 
-class MainActivity : AudioServiceActivity() { }
+class MainActivity : AudioServiceActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        try {
+            val file = File(getExternalFilesDir(null), "tracks.json")
+            file.writeText("""
+hehe pp""")
+            println("File written successfully to: ${file.absolutePath}")
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
