faef579f7e0ade78da949a4e6dbb5b337f553a59
Maximilian Berkes <heggo@heggo.dev>
2026-05-21 12:42:01 +0200

    chore: update dependencies and fix up stuff

--- packages/backend/db/db.ts
+++ packages/backend/db/db.ts
@@ -1,11 +1,10 @@
 // Drizzle & postgres
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from './schema.ts';
 import { relations } from './relations.ts';
 
 let DATABASE_URL = process.env.DATABASE_URL!;
 
 export const pgClient = postgres(DATABASE_URL);
 
-export const db = drizzle(DATABASE_URL, { schema, relations });
+export const db = drizzle(DATABASE_URL, { relations });
